e2968b8b0d643bf31408b9887fd71c40003e7175
Francis Yvan Jubera <francisyvanjubera@gmail.com>
2024-12-14 12:04:48 +0800

    completed implementation of single line entry feature

--- todo.py
+++ todo.py
@@ -11,76 +11,77 @@ while True:
     user_choice = input("Action: ").lower().strip()
     print()
 
-    match user_choice:
-        case "add":
-            todo = input(user_prompt) + "\n"
-            print(todo.capitalize())
-            print()
+    if "add" in user_choice:
+        todo = user_choice[4:]
+        print(todo.capitalize())
+        print()
 
-            # Regular file access
-            # file = open('resources/data/todos.txt', 'r')
-            # todos = file.readlines()
-            # file.close()
+        # Regular file access
+        # file = open('resources/data/todos.txt', 'r')
+        # todos = file.readlines()
+        # file.close()
 
-            # File access using 'with' context manager
-            # Read text file
-            with open('resources/data/todos.txt', 'r') as file:
-                todos = file.readlines()
+        # File access using 'with' context manager
+        # Read text file
+        with open('resources/data/todos.txt', 'r') as file:
+            todos = file.readlines()
             
-            todos.append(todo)
+        todos.append(todo)
 
-            # Write the added item to text file
-            with open('resources/data/todos.txt', 'w') as file:
-                file.writelines(todos)
+        # Write the added item to text file
+        with open('resources/data/todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case "show":
-            # file access
-            with open('resources/data/todos.txt', 'r') as file:
-                todos = file.readlines()
+    elif "show" in user_choice:
+        # file access
+        with open('resources/data/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            for index, item in enumerate(todos):
+        for index, item in enumerate(todos):
                 item = item.strip('\n')
                 print(f"{index + 1}. {item.capitalize()}")
 
-            print()
+        print()
 
-        case "edit":
-            list_number = int(input("Please enter the list no. of the item you want to edit: "))
-            list_number = list_number - 1
-            # Read text file
-            with open('resources/data/todos.txt', 'r') as file:
-                todos = file.readlines()
+    elif "edit" in user_choice:
+        list_number = int(user_choice[5:])
+        list_number = list_number - 1
+        # Read text file
+        with open('resources/data/todos.txt', 'r') as file:
+            todos = file.readlines()
             
-            item_to_edit = todos[list_number]
-            print(f"This is the item you want to replace -> '{item_to_edit.strip('\n')}'")
+        item_to_edit = todos[list_number]
+        print(f"This is the item you want to replace -> '{item_to_edit.strip('\n')}'")
 
-            new_item = input("Enter a replacement for the chosen item: ")
-            todos[list_number] = new_item + '\n'
+        new_item = input("Enter a replacement for the chosen item: ")
+        todos[list_number] = new_item + '\n'
 
-            # Write changes to text file
-            with open('resources/data/todos.txt', 'w') as file:
-                file.writelines(todos)
+        # Write changes to text file
+        with open('resources/data/todos.txt', 'w') as file:
+            file.writelines(todos)
 
             print()
-        case "done":
-            list_no = int(input("Please enter the list no. of the item you want to mark as complete: "))
+    elif "done" in user_choice:
+        list_no = int(user_choice[5:])
 
-            # Read text file
-            with open('resources/data/todos.txt', 'r') as file:
-                todos = file.readlines()
+        # Read text file
+        with open('resources/data/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            list_item = todos.pop(list_no - 1)
+        list_item = todos.pop(list_no - 1)
 
-            # Write changes to the text file
-            with open('resources/data/todos.txt', 'w') as file:
-                file.writelines(todos)
+        # Write changes to the text file
+        with open('resources/data/todos.txt', 'w') as file:
+            file.writelines(todos)
             
-            print(f"item no.{list_no} with the content: '{list_item.strip('\n')}' has been marked as complete and " +
-                  "removed from the list.")
-            # print("removed from the list.")
-            print()
-        case "q":
-            break
+        print(f"item no.{list_no} with the content: '{list_item.strip('\n')}' has been marked as complete and " +
+              "removed from the list.")
+        # print("removed from the list.")
+        print()
+    elif "q" in user_choice:
+        break
+    else:
+        print("You have entered an unknown command!")
             
 print("Goodbye!")
 
