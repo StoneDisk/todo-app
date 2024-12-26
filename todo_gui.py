845faf4c329965c77fb9cd5d3c63484d8b4b5141
Francis Yvan Jubera <francisyvanjubera@gmail.com>
2024-12-26 10:04:52 +0800

    Handle errors for edit and complete features, add clock feature, change gui theme

--- todo_gui.py
+++ todo_gui.py
@@ -1,10 +1,14 @@
 import todosutils as tu
 import FreeSimpleGUI as sg
+import time
 
 
-label = sg.Text("Enter a task")
+sg.theme("DarkGrey15")
+
+clock_label = sg.Text("", key="clock")
+task_input_label = sg.Text("Enter a task")
 input_box = sg.InputText(tooltip="Type task here", key="user_input")
-add_button = sg.Button("Add")
+add_button = sg.Button("Add", size=10)
 
 item_list = tu.get_todos()
 list_box = sg.Listbox(values=item_list, 
@@ -15,15 +19,17 @@ complete_button = sg.Button("Complete")
 
 exit_button = sg.Button("Exit", key="Quit")
 
-layout = [[label], 
+layout = [[clock_label], 
+          [task_input_label], 
           [input_box, add_button], 
           [list_box, edit_button, complete_button],
           [exit_button]]
 
-window = sg.Window("Todo App", layout, font=("Helvetica", 16))
+window = sg.Window("Todo App", layout, font=("Helvetica", 14))
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=200)
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     print(event)
     print(values)
 
@@ -37,24 +43,30 @@ while True:
             todos.append(user_todo)
             tu.write_todos(todos)
             window['items'].update(values=todos)
-        case 'Edit': # incomplete case block, do not trigger!
-            selected_item = values['items'][0]
-            print(selected_item)
+        case 'Edit':
+            try:
+                selected_item = values['items'][0]
+                print(selected_item)
 
-            rep_item = values["user_input"]
+                rep_item = values["user_input"]
 
-            todos = tu.get_todos()
-            index = todos.index(selected_item)
-            todos[index] = rep_item
-            tu.write_todos(todos)
-            window['items'].update(values=todos)
+                todos = tu.get_todos()
+                index = todos.index(selected_item)
+                todos[index] = rep_item
+                tu.write_todos(todos)
+                window['items'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first.", font=("Helvetica", 16))
         case 'Complete':
-            item_to_remove = values['items'][0]
-            todo_list = tu.get_todos()
-            todo_list.remove(item_to_remove)
-            tu.write_todos(todo_list)
-            window['items'].update(values=todo_list)
-            window['user_input'].update(value="")
+            try:
+                item_to_remove = values['items'][0]
+                todo_list = tu.get_todos()
+                todo_list.remove(item_to_remove)
+                tu.write_todos(todo_list)
+                window['items'].update(values=todo_list)
+                window['user_input'].update(value="")
+            except IndexError:
+                sg.popup("Please select an item first.", font=("Helvetica", 16))
         case 'items':
             selected_item = values['items'][0]
             # mod_item = selected_item.split(' ', 1)[1]
